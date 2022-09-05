63c7858eb3bcb8d6ed721e1cf9777ed7f9ebcbd7
nohyunha95 <nohyunha95@gmail.com>
2022-09-05 22:58:33 +0900

    naked return, defer

---
+++ src/github.com/nomad_go/learngo/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int { // 매개변수의 타입이 같다면 뒤에 것만 적어주면 된다.
+	return a * b
+}
+
+// func lenAndUpper(name string) (int, string) { // 다른 타입 두개를 리턴할 수 있다.
+// 	return len(name), strings.ToUpper(name)
+// }
+
+func lenAndUpper(name string) (length int, uppercase string) { // naked return
+	defer fmt.Println("I'm done") // defer는 function이 값을 리턴하고 나면 실행된다.
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
+func repeatMe(words ...string) { // 원하는 만큼 인자를 전달하는 방법
+	fmt.Println(words)
+}
+
+func main() {
+	name := "nico" // 축약형은 오로지 func안에서만 가능하고 변수에만 적용 가능하다.
+	// var name string = "nico"  // 위 코드와 동일
+	name = "Lynn"
+	fmt.Println(name)
+
+	fmt.Println(multiply(2, 2))
+
+	// totalLength, upperName := lenAndUpper("nico")
+	totalLength, _ := lenAndUpper("nico") // 변수명에 _를 줘서 무시하는 값으로 사용할 수 있다
+	fmt.Println(totalLength)
+
+	repeatMe("nico", "lynn", "dal", "marl", "flynn")
+}
